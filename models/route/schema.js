89cb69b92ea4ab72d3459a858e3bfa73140a7c19
thanawat <tha_chookes@hotmail.com>
2016-05-03 10:28:15 +0700

    edit

---
+++ models/route/schema.js
@@ -0,0 +1,17 @@
+// ;(function () {
+//   'use strict'
+//   var modelName = 'snmp'
+//   var mongoose = require('mongoose')
+//   var Schema = mongoose.Schema
+//
+//   var schema = new Schema({
+//     username: String,
+//     password: String,
+//     name: String,
+//     last_name: String,
+//     date: { type: Date, default: Date.now }
+//
+//   })
+//
+//   module.exports = mongoose.model(modelName, schema)
+// })()
--- models/route/snmp.js
+++ models/route/snmp.js
@@ -4,10 +4,13 @@
   var speedTest = require('speedtest-net')
   var router = express.Router()
   var snmp = require('snmp-native')
+  var moment = require('moment')
   var host = '10.41.160.1'
   var community = 'public'
   const scanner = require('node-wifi-scanner')
-
+  function timestamp(time){
+      return (time/360000 |0)
+  }
   router.get('/detail', function (req, res, next) {
     var session = new snmp.Session({ host: host, community: community })
     var oid = [1, 3, 6, 1, 2, 1, 1, 1, 0]
@@ -16,26 +19,40 @@
         console.log(err)
       } else {
         this.vb = varbinds[0]
+        res.send(this.vb.value)
         console.log('The system description is "' + this.vb.value + '"')
       }
       session.close()
     })
   })
-
   router.get('/interface', function (req, res, next) {
+    var value = []
     var session = new snmp.Session({ host: host, community: community })
-    var oid = [1, 3, 6, 1, 2, 1, 1, 1, 0]
-    session.get({ oid: oid }, function (err, varbinds) {
+    session.getSubtree({ oid: '.1.3.6.1.2.1.2.2.1.2' }, function (error, varbinds) {
+      if (error) {
+        console.log('Fail :(')
+      } else {
+        varbinds.forEach(function (vb) {
+          value.push(vb.value)
+          console.log(vb.oid + ' = ' + vb.value + ' (' + vb.type + ')')
+        })
+        res.send(value)
+      }
+    })
+  })
+  router.get('/uptime', function (req, res, next) {
+    var session = new snmp.Session({ host: host, community: community })
+    session.get({ oid: '.1.3.6.1.2.1.1.3.0' }, function (err, varbinds) {
       if (err) {
         console.log(err)
       } else {
         this.vb = varbinds[0]
-        console.log('The system description is "' + this.vb.value + '"')
+        console.log(timestamp(this.vb.value))
+        console.log('uptime"' + this.vb.value + '"')
       }
       session.close()
     })
   })
-
   router.get('/speed', function (req, res, next) {
     var test = speedTest({maxTime: 8000})
     test.on('data', function (data) {
@@ -46,7 +63,6 @@
       console.error(err)
     })
   })
-
   router.get('/wifi', function (req, res, next) {
     scanner.scan((err, networks) => {
       if (err) {
@@ -57,6 +73,5 @@
       res.send(networks)
     })
   })
-
   module.exports = router
 })()
